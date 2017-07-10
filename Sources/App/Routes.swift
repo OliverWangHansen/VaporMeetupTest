91b221f729b01f3a46ff91d009116aac319a3abc
Oliver Wang Hansen <olha@nodesagency.com>
2017-07-10 11:42:00 +0100

    Updated routes

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -11,6 +11,10 @@ extension Droplet {
         get("plaintext") { req in
             return "Hello, world!"
         }
+        
+        get("meetup") { req in
+            return "Thanks!"
+        }
 
         // response to requests to /info domain
         // with a description of the request
